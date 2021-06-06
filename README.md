fed9ac79d2e7db8df836ae409511de15d72f2241
Srikar Manikonda <avatsara3@gmail.com>
2021-06-06 03:16:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Greentrack
+ 
---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+(function() {   // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBpgRjO02cPmxbW0Y4TA5vHiUT_YgqvyaQ",
+    authDomain: "greentrack-201ec.firebaseapp.com",
+    projectId: "greentrack-201ec",
+    storageBucket: "greentrack-201ec.appspot.com",
+    messagingSenderId: "793106418490",
+    appId: "1:793106418490:web:99268e73d9db000c1dde03",
+    measurementId: "G-NJ9YMFGJ8S"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const exampleInputEmail = document.getElementById('exampleInputEmail')
+  const exampleInputPassword = document.getElementById('exampleInputPassword')
+  const 
+
+}());
